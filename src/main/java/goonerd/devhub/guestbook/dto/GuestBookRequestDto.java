5f6c9c02f46d43b07643e68ac942feac7761313b
Goonerd17 <tlghks1217@naver.com>
2025-11-09 23:47:27 +0900

    [feature] Dockerfile 추가

--- src/main/java/goonerd/devhub/guestbook/dto/GuestBookRequestDto.java
+++ src/main/java/goonerd/devhub/guestbook/dto/GuestBookRequestDto.java
@@ -3,12 +3,15 @@ package goonerd.devhub.guestbook.dto;
 import goonerd.devhub.guestbook.entity.GuestBook;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Getter
 @NoArgsConstructor
 public class GuestBookRequestDto {
 
+    @NonNull
     private String username;
+    @NonNull
     private String description;
 
     public GuestBook toEntity() {
