6229339d9b344eeaae5c23bacebbd4f92af64a21
Goonerd17 <tlghks1217@naver.com>
2025-11-10 19:47:45 +0900

    [fix] update Readme

--- src/main/java/goonerd/devhub/guestbook/controller/GuestBookController.java
+++ src/main/java/goonerd/devhub/guestbook/controller/GuestBookController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/guestbook")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://devhub.local")
 public class GuestBookController {
 
     private final GuestBookService guestBookService;
